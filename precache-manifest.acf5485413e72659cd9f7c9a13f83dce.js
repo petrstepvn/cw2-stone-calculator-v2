9d1f416daa31d0660ce9965c9cfb9ed77594df6f
petrstepvn <petrstepvn@gmail.com>
2020-10-19 15:14:25 +0200

    Updates

---
+++ precache-manifest.acf5485413e72659cd9f7c9a13f83dce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f4dafbbc7f71a2d24cd90067fa0f5a1",
+    "url": "/cw2-stone-calculator-v2/index.html"
+  },
+  {
+    "revision": "cf4c847401383fa6a361",
+    "url": "/cw2-stone-calculator-v2/static/css/main.fb5e79b7.chunk.css"
+  },
+  {
+    "revision": "b2bb5aeca5f5dfed753f",
+    "url": "/cw2-stone-calculator-v2/static/js/2.b93ab7b6.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/cw2-stone-calculator-v2/static/js/2.b93ab7b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf4c847401383fa6a361",
+    "url": "/cw2-stone-calculator-v2/static/js/main.888b81a5.chunk.js"
+  },
+  {
+    "revision": "f8411aa79d089d77c362",
+    "url": "/cw2-stone-calculator-v2/static/js/runtime-main.1bef5e37.js"
+  }
+]);
